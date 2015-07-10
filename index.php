63a6d77e9658c7b5db45aa344d40e1eae554fbc9
PrestaEdit <j.danse@prestaedit.com>
2015-07-10 12:53:02 +0200

    [-] PSR-2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 * 2015 PrestaEdit
 *
 *  @author PrestaEdit <j.danse@prestaedit.com>
-*  @license	http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*  @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 */
 
 ini_set('display_errors', 'on');
@@ -13,74 +13,68 @@ require_once('classes/ApplicationException.php');
 require_once('classes/Tools.php');
 require_once('classes/Zip.php');
 
-try
-{
-	$uniqid = false;
-	$zip_name = '';
-	$nb_files_added = 0;
+try {
+    $uniqid = false;
+    $zip_name = '';
+    $nb_files_added = 0;
 
-	if (Tools::isSubmit('checkZip'))
-	{
-		// Save document on disk
-		if (isset($_FILES['zip_file']) && isset($_FILES['zip_file']['tmp_name']) && !empty($_FILES['zip_file']['tmp_name']))
-		{
-			if (pathinfo($_FILES['zip_file']['name'], PATHINFO_EXTENSION) != 'zip')
-	 			throw new ApplicationException('Invalid extension.');
+    if (Tools::isSubmit('checkZip')) {
+        // Save document on disk
+        if (isset($_FILES['zip_file']) && isset($_FILES['zip_file']['tmp_name']) && !empty($_FILES['zip_file']['tmp_name'])) {
+            if (pathinfo($_FILES['zip_file']['name'], PATHINFO_EXTENSION) != 'zip') {
+                throw new ApplicationException('Invalid extension.');
+            }
 
-	 		$zip_name = $_FILES['zip_file']['name'];
+            $zip_name = $_FILES['zip_file']['name'];
 
-			// Generate an uniqid
-			do $uniqid = sha1(microtime());
-			while (file_exists(dirname(__FILE__).'/tmp/'.$uniqid.'.zip'));
+            // Generate an uniqid
+            do {
+                $uniqid = sha1(microtime());
+            } while (file_exists(dirname(__FILE__).'/tmp/'.$uniqid.'.zip'));
 
-			if (!move_uploaded_file($_FILES['zip_file']['tmp_name'], dirname(__FILE__).'/tmp/'.$uniqid.'.zip'))
-				return false;
-		}
-		else
-	 		throw new ApplicationException('Failed to copy the file.');
+            if (!move_uploaded_file($_FILES['zip_file']['tmp_name'], dirname(__FILE__).'/tmp/'.$uniqid.'.zip')) {
+                return false;
+            }
+        } else {
+            throw new ApplicationException('Failed to copy the file.');
+        }
 
-		// Check Zip
-		$zip = new ZipArchive();
+        // Check Zip
+        $zip = new ZipArchive();
 
-		$result_code = $zip->open(dirname(__FILE__).'/tmp/'.$uniqid.'.zip');
+        $result_code = $zip->open(dirname(__FILE__).'/tmp/'.$uniqid.'.zip');
 
-		if ($result_code === true)
-		{
-			for ($i = 0; $i < $zip->numFiles; $i++)
-			{
-			$stats = $zip->statIndex($i);
-				if (!$stats['size'] > 0)
-				{
-					$filename = $zip->getNameIndex($i).'index.php';
-					if ($zip->locateName($filename) === false)
-					{
-						$zip->addFromString($filename, Tools::getDefaultIndexContent());
-						$nb_files_added++;
-					}
-				}
-			}
+        if ($result_code === true) {
+            for ($i = 0; $i < $zip->numFiles; $i++) {
+                $stats = $zip->statIndex($i);
+                if (!$stats['size'] > 0) {
+                    $filename = $zip->getNameIndex($i).'index.php';
+                    if ($zip->locateName($filename) === false) {
+                        $zip->addFromString($filename, Tools::getDefaultIndexContent());
+                        $nb_files_added++;
+                    }
+                }
+            }
 
-			// Close
-			$zip->close();
+            // Close
+            $zip->close();
 
-			echo $nb_files_added.' index added. <a href="download.php?uniqid='.$uniqid.'&filename='.$zip_name.'" target="_blank">[download]</a> - <a href="index.php?deleteZip=1&uniqid='.$uniqid.'">[delete]</a><br /><br />------<br /><br />';
-		}
-	 	else
-	 		throw new ApplicationException('Error. '.Zip::displayError((int)$result_code));
-	}
-	else if (Tools::isSubmit('deleteZip'))
-	{
-		$uniqid = Tools::getValue('uniqid', 0);
+            echo $nb_files_added.' index added. <a href="download.php?uniqid='.$uniqid.'&filename='.$zip_name.'" target="_blank">[download]</a> - <a href="index.php?deleteZip=1&uniqid='.$uniqid.'">[delete]</a><br /><br />------<br /><br />';
+        } else {
+            throw new ApplicationException('Error. '.Zip::displayError((int)$result_code));
+        }
+    } elseif (Tools::isSubmit('deleteZip')) {
+        $uniqid = Tools::getValue('uniqid', 0);
 
-		if ($uniqid)
-			Tools::deleteFile(dirname(__FILE__).'/tmp/'.$uniqid.'.zip');
-	}
-}
-catch (ApplicationException $ex)
-{
-	if ($uniqid !== false)
-		Tools::deleteFile(dirname(__FILE__).'/tmp/'.$uniqid.'.zip');
-	$ex->displayMessage();
+        if ($uniqid) {
+            Tools::deleteFile(dirname(__FILE__).'/tmp/'.$uniqid.'.zip');
+        }
+    }
+} catch (ApplicationException $ex) {
+    if ($uniqid !== false) {
+        Tools::deleteFile(dirname(__FILE__).'/tmp/'.$uniqid.'.zip');
+    }
+    $ex->displayMessage();
 }
 
 // Show form
